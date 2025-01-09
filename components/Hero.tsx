d5b877e3e4e38a16b4ceb89f176c0de423620425
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-01-09 23:57:37 +0100

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
       </div>
       <div className="absolute top-0 z-0 w-full h-screen">
         <Image
-          src="/images/marissa-grootes-ao6N4qjR9FM-unsplash.jpg"
+          src="/images/seth-schwiet-WB3ujiKLJwQ-unsplash.jpg"
           alt="Background image"
           fill
           objectFit="cover"
