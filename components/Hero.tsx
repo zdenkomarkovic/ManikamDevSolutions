3b1fe348044b89f811f73880e9e7c772f5fcc6ae
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-02-09 01:37:55 +0100

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,63 +1,63 @@
 "use client";
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 import Image from "@/node_modules/next/image";
 import React from "react";
 
-function FloatingPaths({ position }: { position: number }) {
-  const paths = Array.from({ length: 36 }, (_, i) => ({
-    id: i,
-    d: `M-${380 - i * 5 * position} -${189 + i * 6}C-${
-      380 - i * 5 * position
-    } -${189 + i * 6} -${312 - i * 5 * position} ${216 - i * 6} ${
-      152 - i * 5 * position
-    } ${343 - i * 6}C${616 - i * 5 * position} ${470 - i * 6} ${
-      684 - i * 5 * position
-    } ${875 - i * 6} ${684 - i * 5 * position} ${875 - i * 6}`,
-    color: `rgba(15,23,42,${0.1 + i * 0.03})`,
-    width: 0.5 + i * 0.03,
-  }));
+// function FloatingPaths({ position }: { position: number }) {
+//   const paths = Array.from({ length: 36 }, (_, i) => ({
+//     id: i,
+//     d: `M-${380 - i * 5 * position} -${189 + i * 6}C-${
+//       380 - i * 5 * position
+//     } -${189 + i * 6} -${312 - i * 5 * position} ${216 - i * 6} ${
+//       152 - i * 5 * position
+//     } ${343 - i * 6}C${616 - i * 5 * position} ${470 - i * 6} ${
+//       684 - i * 5 * position
+//     } ${875 - i * 6} ${684 - i * 5 * position} ${875 - i * 6}`,
+//     color: `rgba(15,23,42,${0.1 + i * 0.03})`,
+//     width: 0.5 + i * 0.03,
+//   }));
 
-  return (
-    <div className="absolute inset-0 pointer-events-none">
-      <svg
-        className="w-full h-full text-slate-950 dark:text-white"
-        viewBox="0 0 696 316"
-        fill="none"
-      >
-        {paths.map((path) => (
-          <motion.path
-            key={path.id}
-            d={path.d}
-            stroke="currentColor"
-            strokeWidth={path.width}
-            strokeOpacity={0.1 + path.id * 0.03}
-            initial={{ pathLength: 0.3, opacity: 0.6 }}
-            animate={{
-              pathLength: 1,
-              opacity: [0.3, 0.6, 0.3],
-              pathOffset: [0, 1, 0],
-            }}
-            transition={{
-              duration: 20 + Math.random() * 10,
-              repeat: Number.POSITIVE_INFINITY,
-              ease: "linear",
-            }}
-          />
-        ))}
-      </svg>
-    </div>
-  );
-}
+//   return (
+//     <div className="absolute inset-0 pointer-events-none">
+//       <svg
+//         className="w-full h-full text-slate-950 dark:text-white"
+//         viewBox="0 0 696 316"
+//         fill="none"
+//       >
+//         {paths.map((path) => (
+//           <motion.path
+//             key={path.id}
+//             d={path.d}
+//             stroke="currentColor"
+//             strokeWidth={path.width}
+//             strokeOpacity={0.1 + path.id * 0.03}
+//             initial={{ pathLength: 0.3, opacity: 0.6 }}
+//             animate={{
+//               pathLength: 1,
+//               opacity: [0.3, 0.6, 0.3],
+//               pathOffset: [0, 1, 0],
+//             }}
+//             transition={{
+//               duration: 20 + Math.random() * 10,
+//               repeat: Number.POSITIVE_INFINITY,
+//               ease: "linear",
+//             }}
+//           />
+//         ))}
+//       </svg>
+//     </div>
+//   );
+// }
 
 const Hero = () => {
   return (
     <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
-      <div className="absolute inset-0 z-10">
+      {/* <div className="absolute inset-0 z-10">
         <FloatingPaths position={1} />
         <FloatingPaths position={-1} />
-      </div>
+      </div> */}
       <div className="container mx-auto px-[5px] md:px-4 h-screen flex items-center">
         <div className="">
           <h1 className="relative text-gray-900 mt-[250px] md:mt-0 text-[33px] md:text-6xl xl:text-[80px] z-20">
