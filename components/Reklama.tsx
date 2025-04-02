4122d22aecf26781dafa7899b44047fc42ceacde
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-04-02 03:15:19 +0200

    .

--- components/Reklama.tsx
+++ components/Reklama.tsx
@@ -5,7 +5,7 @@ const Reklama = () => {
   return (
     <div className="relative min-h-screen w-full flex items-center justify-center overflow-hidden bg-white dark:bg-neutral-950">
       <div className="container mx-auto px-[5px] md:px-4 h-screen flex flex-col items-center">
-        <div className="z-20 space-y-5 text-xl mt-10">
+        <div className="z-20 space-y-5 text-xl mt-10 font-bold">
           <h2 className="z-20 font-bold text-red-700 text-3xl">
             Sajt za samo 32.000 RSD!
           </h2>
@@ -24,13 +24,11 @@ const Reklama = () => {
               mi pomazemo vasem biznisu da raste
             </span>
           </h1>
-          <p className="mt-[10px] text-red-700 text-center">
-            Nemate materijal?
-          </p>
+          <p className="mt-[10px] text-center font-bold">Nemate materijal?</p>
           <p className="text-lg">
             Nema problema – pišemo tekstove i dodajemo profi slike!
           </p>
-          <p className=" text-red-700 text-center">Brži i kvalitetniji sajt!</p>
+          <p className=" text-center font-bold">Brži i kvalitetniji sajt!</p>
           <p className="text-lg">
             Radimo u naprednoj tehnologiji programiranja (Next.js) – Ne
             koristimo spore page buildere!
