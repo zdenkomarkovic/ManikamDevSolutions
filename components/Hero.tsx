70d131446cb69a0296d2a1316b3000cf87ecfc1c
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-09-03 00:49:52 +0200

    .

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -50,8 +50,8 @@ const Hero = ({ title }: HeroProps) => {
   }, [services.length]);
 
   return (
-    <div className="relative max-h-screen w-full bg-white dark:bg-neutral-950 overflow-hidden">
-     <div className="relative container mx-auto px-[5px] md:px-4 min-h-screen overflow-hidden">
+    <div className="relative h-[100dvh] w-full bg-white dark:bg-neutral-950 overflow-hidden">
+     <div className="relative container mx-auto px-[5px] md:px-4 h-[100dvh] overflow-hidden">
       {/* H1 naslov - pozicioniran apsolutno */}
       <div className="absolute bottom-16 left-3 md:left-24 z-20">
         <h1 className="relative text-gray-900 text-[33px] md:text-6xl xl:text-[80px]">
