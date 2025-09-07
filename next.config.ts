bbb4a0a6b718a1834f56e74f6afb3b9cc4cc0fa6
Zdenko Markovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-09-07 15:11:21 +0200

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    suppressHydrationWarnings: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
