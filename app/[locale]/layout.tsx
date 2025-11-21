9091f89f45b0e367f935e840bab668f56c9997e6
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-11-21 20:29:31 +0100

    .

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -8,6 +8,7 @@ import { isValidLocale } from "@/lib/locale";
 import { LocaleProvider } from "../../lib/LocaleContext";
 import { Metadata } from "@/node_modules/next/types";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -75,6 +76,21 @@ export default async function LocaleLayout({
         suppressHydrationWarning={true}
       >
         <GoogleAnalytics />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '870815125416740');
+    fbq('track', 'PageView');
+  `}
+        </Script>
+
         <LocaleProvider locale={locale}>
           <Header locale={locale} />
           {children}
