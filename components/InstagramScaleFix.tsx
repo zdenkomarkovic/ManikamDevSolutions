7856709ba576f196dd6cd6396865a7eaddfd805a
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-12-05 03:00:03 +0100

    .

--- components/InstagramScaleFix.tsx
+++ components/InstagramScaleFix.tsx
@@ -5,7 +5,7 @@ export default function InstagramScaleFix() {
   useEffect(() => {
     const ua = navigator.userAgent || "";
     if (ua.includes("Instagram")) {
-      document.documentElement.style.setProperty("--ig-scale", "0.80");
+      document.documentElement.style.zoom = "0.80";
     }
   }, []);
 
