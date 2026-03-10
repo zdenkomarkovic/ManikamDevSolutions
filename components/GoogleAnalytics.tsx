c93030bf260c0a4688bcffc9b73719cf90e0677e
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2026-03-10 02:17:39 +0100

    .

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -7,7 +7,7 @@ const GOOGLE_ADS_TAG_ID = process.env.NEXT_PUBLIC_GOOGLE_ADS_TAG_ID || "";
 const GoogleAnalytics = () => (
   <>
     <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_TAG_ID || GA_MEASUREMENT_ID}`}
       strategy="afterInteractive"
     />
     <Script id="google-analytics" strategy="afterInteractive">
